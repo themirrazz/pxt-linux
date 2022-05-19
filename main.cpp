54f3a49d0508f27d059ab51a0a6ff7ec50e386eb
themirrazz <89490659+themirrazz@users.noreply.github.com>
2022-05-19 10:05:00 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,13 @@
 #include "MicroBit.h"
 #include "pxt.h"
+
+using namespace pxt
+
 //% color=#E3008C weight=96 icon="\uf012"
 namespace linux {
-    //todo
+    //% blockId=linuxBlock block="linux block"
+    //% weight=100
+    void linuxBlock() {
+        uBit.display.scroll("this is a linux block")
+    }
 }
