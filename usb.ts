e6dd685f65450322fe4df816dde6a36fc2040633
themirrazz <89490659+themirrazz@users.noreply.github.com>
2022-05-19 12:27:35 -0700

    Update pxt.json, main.blocks, main.ts, usb.ts

---
+++ usb.ts
@@ -0,0 +1,9 @@
+// Add your code here
+
+//% block="USB3" icon="\uf287" color=#f6aaf4
+namespace usb {
+    //% block="init USB 1\nTX %tx\nRX %rx"
+    export function initUsb1(tx:DigitalPin,rx:DigitalPin) {
+        //todo
+    }
+}
