aa5815fc9e12a0ef5fe85a20698f8cc7fe13b8bb
themirrazz <89490659+themirrazz@users.noreply.github.com>
2022-05-19 09:51:24 -0700

    Update main.cpp, enums.d.ts

--- enums.d.ts
+++ enums.d.ts
@@ -1,5 +1,5 @@
 // Auto-generated. Do not edit.
-declare namespace rad {
+declare namespace linux {
 }
 
 // Auto-generated. Do not edit. Really.
--- main.cpp
+++ main.cpp
@@ -1,9 +1,6 @@
 #include "MicroBit.h"
 #include "pxt.h"
 //% color=#E3008C weight=96 icon="\uf012"
-namespace rad {
-    //% blockId=radHuhBlock block="huh"
-    int xyz() {
-        return 0
-    }
+namespace linux {
+    //todo
 }
