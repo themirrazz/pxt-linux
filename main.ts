b2c839c6957d72c880588135f99d34f62c56415d
themirrazz <89490659+themirrazz@users.noreply.github.com>
2022-05-19 10:44:06 -0700

    Update pxt.json, main.ts

--- main.ts
+++ main.ts
@@ -1,4 +1,11 @@
-basic.showString(linux.whatTheBruh())
-basic.forever(function () {
-	
-})
+//% block="Linux" icon="\uf120" color=#a053b8
+namespace linux {
+    //% block="what the bruh?"
+    export function whatTheBruh() {
+        return bruh()
+    }
+    //% shim=linux::bruh
+    function bruh() {
+        return "fake-bruh"
+    }
+}
