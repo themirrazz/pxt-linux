80490a15637a93384e70e0b0c27977f66f0df333
themirrazz <89490659+themirrazz@users.noreply.github.com>
2022-05-19 09:35:55 -0700

    Update pxt.json, main.blocks, main.ts, main.cpp, shims.d.ts

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include "MicroBit.h"
+#include "pxt.h"
 //% color=#E3008C weight=96 icon="\uf012"
 namespace rad {
     //% blockId=radHuhBlock block="huh"
--- shims.d.ts
+++ shims.d.ts
@@ -1,9 +1,30 @@
-// Auto-generated. Do not edit.
-
-
-
-    //% color=#E3008C weight=96 icon="\uf012"
-declare namespace rad {
+{
+    "name": "pxt-linux",
+    "version": "0.0.0",
+    "description": "",
+    "dependencies": {
+        "core": "*",
+        "radio": "*",
+        "microphone": "*"
+    },
+    "files": [
+        "README.md",
+        "main.blocks",
+        "main.ts",
+        "main.cpp",
+        "enums.d.ts",
+        "shims.d.ts"
+    ],
+    "testFiles": [
+        "test.ts"
+    ],
+    "public": true,
+    "targetVersions": {
+        "target": "4.0.18",
+        "targetId": "microbit"
+    },
+    "supportedTargets": [
+        "microbit"
+    ],
+    "preferredEditor": "tsprj"
 }
-
-// Auto-generated. Do not edit. Really.
